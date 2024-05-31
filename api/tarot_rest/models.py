42e2261e5a1c93aacdac6acce5b0f8f8f9b6d97d
Adrianna <adrianna.j.fowler@gmail.com>
2024-05-31 15:28:51 -0400

    models created

--- api/tarot_rest/models.py
+++ api/tarot_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=150)
+    exclude_negative = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=150)
+    image_url = models.URLField()
+    description = models.TextField()
+    is_negative = models.BooleanField(default=False)
+    deck = models.ForeignKey(
+        Deck,
+        related_name="cards",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
